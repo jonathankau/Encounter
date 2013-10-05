ac9c6f8cede613950de040c0da2251e34c7cac99
Jonathan Kau <jkau15@gmail.com>
2013-10-05 14:23:51 -0400

    Adding in random num generation

--- src/com/moco/magnet/encounter/CreateActivity.java
+++ src/com/moco/magnet/encounter/CreateActivity.java
@@ -1,15 +1,26 @@
 package com.moco.magnet.encounter;
 
+import java.util.Random;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class CreateActivity extends Activity {
+	int rand_num = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create);
+		
+		Random r = new Random();
+		rand_num = r.nextInt(1001);
+		String new_code = String.format("%04d", rand_num);
+		
+		TextView code = (TextView) CreateActivity.this.findViewById(R.id.new_pin);
+		code.setText(new_code);
 	}
 
 	@Override
