ec4dcee5cec97870f7b2c6bddb6c783a279a55e4
Jonathan Kau <jkau15@gmail.com>
2013-10-05 13:49:44 -0400

    Added basic activities, buttons

--- src/com/moco/magnet/encounter/CreateActivity.java
+++ src/com/moco/magnet/encounter/CreateActivity.java
@@ -1,17 +1,15 @@
 package com.moco.magnet.encounter;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 
-public class MainActivity extends Activity {
+public class CreateActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_create);
 	}
 
 	@Override
@@ -19,17 +17,6 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
-	
-	public void joinRoom(View v) {
-		Intent intent = new Intent(v.getContext(), JoinActivity.class);
-		startActivity(intent);
-	}
-	
-	
-	public void createRoom(View v) {
-		Intent intent = new Intent(v.getContext(), CreateActivity.class);
-		startActivity(intent);
 	}
 
 }
