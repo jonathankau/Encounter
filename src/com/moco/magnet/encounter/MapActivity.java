a0817be90e5eb048403b4fde49243009be0babda
JustinHoUMD <itsjustinn@gmail.com>
2013-10-05 19:44:55 -0400

    Added code to get current location

--- src/com/moco/magnet/encounter/MapActivity.java
+++ src/com/moco/magnet/encounter/MapActivity.java
@@ -21,19 +21,22 @@ public class MapActivity extends FragmentActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_map);
+    MyLocationListener a = new MyLocationListener(null); 
+    a.showCurrentLocation(); 
     map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
         .getMap();
-    Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-        .title("Hamburg"));
-    Marker kiel = map.addMarker(new MarkerOptions()
+    LatLng currLocation = new LatLng(a.location.getLatitude(),a.location.getLongitude()); 
+    Marker location = map.addMarker(new MarkerOptions().position(currLocation)
+        .title("Current Location"));
+    /*Marker kiel = map.addMarker(new MarkerOptions()
         .position(KIEL)
         .title("Kiel")
         .snippet("Kiel is cool")
         .icon(BitmapDescriptorFactory
-            .fromResource(R.drawable.ic_launcher)));
+            .fromResource(R.drawable.ic_launcher)));*/
 
     // Move the camera instantly to hamburg with a zoom of 15.
-    map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+    map.moveCamera(CameraUpdateFactory.newLatLngZoom(currLocation, 15));
 
     // Zoom in, animating the camera.
     map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
