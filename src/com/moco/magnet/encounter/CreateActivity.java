9b8d11a471c972b7bc89246a13e8a9d5686c5a7f
Jonathan Kau <jkau15@gmail.com>
2013-10-05 15:46:18 -0400

    Connecting to Database
    Finished CreateActivity for the most part

--- src/com/moco/magnet/encounter/CreateActivity.java
+++ src/com/moco/magnet/encounter/CreateActivity.java
@@ -4,23 +4,64 @@ import java.util.Random;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.provider.Settings.Secure;
 import android.view.Menu;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.ValueEventListener;
 
 public class CreateActivity extends Activity {
-	int rand_num = 0;
+	private int randNum = 0;
+	private final String sessionsUrl = "https://encounter-sessions.firebaseIO.com/";
+	private final String usersUrl = "https://encounter-users.firebaseIO.com/";
+
+	private Firebase sessions = new Firebase(sessionsUrl);
+	private Firebase users = new Firebase(usersUrl);
+
+	String new_code;
+	boolean foundNewCode = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create);
-		
-		Random r = new Random();
-		rand_num = r.nextInt(1001);
-		String new_code = String.format("%04d", rand_num);
-		
-		TextView code = (TextView) CreateActivity.this.findViewById(R.id.new_pin);
-		code.setText(new_code);
+
+
+		//Debug
+		sessions.addValueEventListener(new ValueEventListener() {
+			@Override
+			public void onDataChange(DataSnapshot snapshot) {
+				if(foundNewCode == false) {
+					Random r = new Random();
+					randNum = r.nextInt(1001);
+					new_code = String.format("%04d", randNum);
+
+					while(snapshot.child(new_code).getValue() != null) {
+						randNum = r.nextInt(1001);
+						new_code = String.format("%04d", randNum);
+					}
+
+					foundNewCode = true;
+					
+					String deviceID = Secure.getString(CreateActivity.this.getContentResolver(),
+							Secure.ANDROID_ID);
+
+					sessions.child(new_code).setValue(deviceID);
+					
+					// Update the actual textview with the new pin
+					TextView code = (TextView) CreateActivity.this.findViewById(R.id.new_pin);
+					code.setText(new_code);
+				}
+			}
+
+			@Override
+			public void onCancelled() {
+				System.err.println("Listener was cancelled");
+			}
+		});
 	}
 
 	@Override
